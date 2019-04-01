66ca0726f0921ab42279006044ab32a52e37baa2
drevincibot <dre.vivero@gmail.com>
2019-04-01 17:50:16 -0400

    updated latency script

--- scripts/latency_test.py
+++ scripts/latency_test.py
@@ -25,68 +25,64 @@ class latencyData:
 
 
     # def scan_callback(self, data):
-    # 	print(data) 
+    # 	print(data)
 
 
     def laserscan_callback(self):
 
     	pub = rospy.Publisher("/ARFUROS/LaserScan", LaserScan, queue_size = 1)
 
+        # Create a for-loop to calculate a moving average for the time latency
     	# Step 1: get original scan message from driver
     	msg =  rospy.wait_for_message("/scan",LaserScan)
-
+        first_msg_id = msg.header.seq
+        time_stamp_1 = msg.header.stamp.secs + msg.header.stamp.nsecs / 1000000000.0
     	print(msg)
 
-
     	# Step 2: make a copy of that message and publish it to AR device
-    		# save time at sending
-    	# rospy.Publisher("/ARFUROS/LaserScan", LaserScan, queue_size = 1)
-
+    	# save time at sending
     	pub.publish(msg)
+        msg_time_sent = rospy.Time.now()
 
-
-    	# Step 3: wait for the message to get back from the AR device -- the AR device should set the 
+    	# Step 3: wait for the message to get back from the AR device -- the AR device should set the
     	# header of the return message to be identical to the header of the message it received
-    		# save time at receiving and compare
+    	# save time at receiving and compare
     	second_msg = rospy.wait_for_message("/ARFUROS/LatencyTest/LaserScan",LaserScan)
-
+        msg_time_recieved = rospy.Time.now()
+        time_stamp_2 = second_msg.header.stamp.secs + second_msg.header.stamp.nsecs / 1000000000.0
+        second_msg_id = second_msg.header.seq
     	print(second_msg)
 
-        # obtain first message from /scan topic
-        """rospy.loginfo("Getting first message...")
-        self.first_ls_msg = rospy.wait_for_message("/ARFUROS/LaserScan",LaserScan)
-        first_msg_id = self.first_ls_msg.header.seq
 
-        time_stamp_1 = self.first_ls_msg.header.stamp.secs + self.first_ls_msg.header.stamp.nsecs / 1000000000.0
-
-        first_msg_time = rospy.Time.now()
-        rospy.loginfo("Got first message!")
-        # print(first_msg_id, time_stamp_1,first_msg_time)
-        print(self.first_ls_msg)
-
-        # obtain first message from /scan topic
-        rospy.loginfo("Waiting for return message...")
-        self.second_ls_msg = rospy.wait_for_message("/ARFUROS/LatencyTest/LaserScan",LaserScan)
-
-        time_stamp_2 = self.second_ls_msg.header.stamp.secs + self.second_ls_msg.header.stamp.nsecs / 1000000000.0
-        second_msg_id = self.second_ls_msg.header.seq
-
-        second_msg_time = rospy.Time.now()
-        rospy.loginfo("Got second message!")
-        # print(second_msg_id, time_stamp_2 ,second_msg_time)
-        print(self.second_ls_msg)"""
-
-
-
-
-        # check if second message is equal or greater than the first message and then calculate dt in seconds
-        """if first_msg_id <= second_msg_id:
-        	# dt = time_stamp_2 - time_stamp_1
-            ls_msg_dt = second_msg_time - first_msg_time
-            seconds_dt = ls_msg_dt / 1e9
-            print("/scan latency is: ", ls_msg_dt)
+        if first_msg_id == second_msg_id:
+            dt = time_stamp_2 - time_stamp_1
+            ls_msg_dt = msg_time_recieved - msg_time_sent
+            print("/scan latency is: ", ls_msg_dt, dt)
         else:
-            print("Error in message id...")"""
+            print("Message ID mismatch...")
+
+        # rospy.loginfo("Getting first message...")
+        # self.first_ls_msg = rospy.wait_for_message("/ARFUROS/LaserScan",LaserScan)
+        # first_msg_id = self.first_ls_msg.header.seq
+        #
+        # time_stamp_1 = self.first_ls_msg.header.stamp.secs + self.first_ls_msg.header.stamp.nsecs / 1000000000.0
+        #
+        # first_msg_time = rospy.Time.now()
+        # rospy.loginfo("Got first message!")
+        # # print(first_msg_id, time_stamp_1,first_msg_time)
+        # print(self.first_ls_msg)
+        #
+        # # obtain first message from /scan topic
+        # rospy.loginfo("Waiting for return message...")
+        # self.second_ls_msg = rospy.wait_for_message("/ARFUROS/LatencyTest/LaserScan",LaserScan)
+        #
+        # time_stamp_2 = self.second_ls_msg.header.stamp.secs + self.second_ls_msg.header.stamp.nsecs / 1000000000.0
+        # second_msg_id = self.second_ls_msg.header.seq
+        #
+        # second_msg_time = rospy.Time.now()
+        # rospy.loginfo("Got second message!")
+        # # print(second_msg_id, time_stamp_2 ,second_msg_time)
+        # print(self.second_ls_msg)
 
 
 if __name__ == '__main__':
