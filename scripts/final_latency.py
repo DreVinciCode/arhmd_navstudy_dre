454b3f49a045a07e826dbabd19b7bef6c56d3b57
drevincibot <dre.vivero@gmail.com>
2019-04-04 22:59:44 -0400

    latency with csv logger

--- scripts/final_latency.py
+++ scripts/final_latency.py
@@ -5,11 +5,15 @@ import rospy
 import math
 import sys, time
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import style
 from std_msgs.msg import Float64, String
 from nav_msgs.msg import Odometry, Path, OccupancyGrid
 from sensor_msgs.msg import LaserScan
 from geometry_msgs.msg import PoseWithCovarianceStamped, Twist, PoseArray
 import csv
+
 
 
 class latencyData:
@@ -19,6 +23,7 @@ class latencyData:
         rospy.init_node('latency_test', anonymous = True)
 
         # variables
+        filenumber = 0
         self.ls_msg_avg = 0.0
         self.cm_msg_avg = 0.0
         self.lz_msg_avg = 0.0
@@ -27,6 +32,20 @@ class latencyData:
         self.counter_cm = 0
         self.counter_lz = 0
         self.counter_ph = 0
+        self.ls_array = []
+        self.cm_array = []
+        self.lz_array = []
+        self.ph_array = []
+
+        style.use('fivethirtyeight')
+
+        fig=plt.figure()
+        ax1 = plt.subplot(411)
+        ax2 = plt.subplot(412)
+        ax3 = plt.subplot(413)
+        ax4 = plt.subplot(414)
+
+        plt.show()
 
         # subscribers
         self.laserscan_return = rospy.Subscriber("/laser_from", LaserScan, self.scan_return_callback)
@@ -35,6 +54,32 @@ class latencyData:
         self.path_return = rospy.Subscriber("/path_from", Path, self.path_return_callback)
 
         rospy.spin()
+
+        #script to write data to csv file
+        with open('laserscan_latency' + str(filenumber)+ ".csv", mode='w') as csv_file:
+            logger = csv.writer(csv_file, delimiter=',')
+
+            for i in range(len(self.ls_array)):
+                logger.writerow([self.ls_array[i])
+
+        with open('costmap_latency' + str(filenumber)+ ".csv", mode='w') as csv_file:
+            logger = csv.writer(csv_file, delimiter=',')
+
+            for i in range(len(self.cm_array)):
+                logger.writerow([self.cm_array[i])
+
+        with open('localization_latency' + str(filenumber)+ ".csv", mode='w') as csv_file:
+            logger = csv.writer(csv_file, delimiter=',')
+
+            for i in range(len(self.lz_array)):
+                logger.writerow([self.lz_array[i])
+
+        with open('path_latency' + str(filenumber)+ ".csv", mode='w') as csv_file:
+            logger = csv.writer(csv_file, delimiter=',')
+
+            for i in range(len(self.ph_array)):
+                logger.writerow([self.ph_array[i])
+
 
     def scan_return_callback(self, data):
 
@@ -47,11 +92,14 @@ class latencyData:
 
         # convert to miliseconds
         ls_msg_dt = (diff_nsecs / 1000000) + (diff_secs * 1000)
+        self.ls_array.append(ls_msg_dt)
 
         # perform moving average
         self.counter_ls = self.counter_ls + 1
         self.ls_msg_avg = (self.counter_ls-1)*(self.ls_msg_avg)/(self.counter_ls) + (1/self.counter_ls)*ls_msg_dt
         self.printout()
+
+
 
     def cost_return_callback(self, data):
 
@@ -63,6 +111,7 @@ class latencyData:
         diff_nsecs = later.nsecs - msg_return.header.stamp.nsecs
 
         cm_msg_dt = (diff_nsecs / 1000000) + (diff_secs * 1000)
+        self.cm_array.append(cm_msg_dt)
 
         self.counter_cm = self.counter_cm + 1
         self.cm_msg_avg = (self.counter_cm-1)*(self.cm_msg_avg)/(self.counter_cm) + (1/self.counter_cm)*cm_msg_dt
@@ -79,6 +128,7 @@ class latencyData:
         diff_nsecs = later.nsecs - msg_return.header.stamp.nsecs
 
         lz_msg_dt = (diff_nsecs / 1000000) + (diff_secs * 1000)
+        self.lz_array.append(lz_msg_dt)
 
         self.counter_lz = self.counter_lz + 1
         self.lz_msg_avg = (self.counter_lz-1)*(self.lz_msg_avg)/(self.counter_lz) + (1/self.counter_lz)*lz_msg_dt
@@ -95,11 +145,12 @@ class latencyData:
         diff_nsecs = later.nsecs - msg_return.header.stamp.nsecs
 
         ph_msg_dt = (diff_nsecs / 1000000) + (diff_secs * 1000)
+        self.ph_array.append(ph_msg_dt)
 
         self.counter_ph = self.counter_ph + 1
         self.ph_msg_avg = (self.counter_ph-1)*(self.ph_msg_avg)/(self.counter_ph) + (1/self.counter_ph)*ph_msg_dt
         self.printout()
-        
+
     def printout(self):
         print("LaserScan [ms]: ", round(self.ls_msg_avg) , "\tCostmap [ms]: " , round(self.cm_msg_avg) , "\tPath [ms] :" , round(self.ph_msg_avg) , "\tLocalization [ms]: ", round(self.lz_msg_avg))
 
